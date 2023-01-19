a3dcb7c3105e2a0a0bd63d48df46c3f673076852
alavirttual <kufnikov.14@gmail.com>
2023-01-20 00:12:40 +0300

    firs commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import telebot
+from telebot import types
+from datetime import date
+import emoji
+import copy
+import json
+
+
+
+def main():
+    bot = telebot.TeleBot('1288987415:AAFkTpWoqYqXJ9yMpuqWSXqsjrGaW1fm24k');
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
