01bc26f025ed18b040cd799541412154a41b46d1
alavirttual <kufnikov.14@gmail.com>
2023-01-21 18:33:06 +0300

    init_bot

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 import telebot
-from telebot import types
-from datetime import date
-import emoji
-import copy
-import json
+
 
 
 
 def main():
     bot = telebot.TeleBot('1288987415:AAFkTpWoqYqXJ9yMpuqWSXqsjrGaW1fm24k');
+    @bot.message_handler(commands=['start'])
+    def start_bot(message):
+        bot.send_message(message.chat.id, 'Привет')
+
+    print("[INFO] Bot is started")
+    bot.infinity_polling()
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     main()
 
 
